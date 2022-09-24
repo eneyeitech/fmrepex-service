855aa273dbbf28cb3988469c42ee8845f2369c44
eneyeitech <abdulmumineabdulkarim@gmail.com>
2022-09-24 12:39:28 +0100

    new 1

--- src/main/java/com/example/fmrepexservice/presentation/api/UserController.java
+++ src/main/java/com/example/fmrepexservice/presentation/api/UserController.java
@@ -1,2 +1,13 @@
-package com.example.fmrepexservice.presentation.api;public class UserController {
+package com.example.fmrepexservice.presentation.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("api/v1/users")
+    public String getUsers(){
+        return "Hello Users.";
+    }
 }
